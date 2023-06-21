a36524d1add7a9e632f87c310056140200214168
ArantesVini <vinicius.daniel02@gmail.com>
2023-06-21 19:22:37 -0300

    feat: refactor the function using "with"

--- dags/extract_load.py
+++ dags/extract_load.py
@@ -22,59 +22,24 @@ default_args = {
 }
 
 def _transfer_data():
-    source_conn = connect(
-        host='source_db',
-        database='postgresDB',
-        user='dbadmin',
-        password='dbadmin123'
-    )
-    source_cursor = source_conn.cursor()
+    with connect(host='source_db', database='postgresDB', user='dbadmin', password='dbadmin123') as source_conn, \
+            connect(host='destiny_db', database='postgresDB', user='dbadmin', password='dbadmin123') as dest_conn:
+        source_cursor = source_conn.cursor()
+        dest_cursor = dest_conn.cursor()
 
-    dest_conn = connect(
-        host='destiny_db',
-        database='postgresDB',
-        user='dbadmin',
-        password='dbadmin123'
-    )
-    dest_cursor = dest_conn.cursor()
+        source_cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='dbs'")
+        tables = [row[0] for row in source_cursor.fetchall()]
 
-    source_cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='dbs'")
-    tables = [row[0] for row in source_cursor.fetchall()]
+        for table in tables:
+            source_cursor.execute(f"SELECT * FROM dbs.{table}")
+            rows = source_cursor.fetchall()
 
-    for table in tables:
-        source_cursor.execute(sql.SQL("SELECT * FROM {}.{}").format(sql.Identifier('dbs'), sql.Identifier(table)))
-        rows = source_cursor.fetchall()
+            dest_table = f"sta.st_{table}"
+            dest_cursor.execute(f"CREATE TABLE IF NOT EXISTS {dest_table} (LIKE dbs.{table} INCLUDING ALL)")
+            dest_cursor.execute(f"TRUNCATE TABLE {dest_table}")
+            execute_values(dest_cursor, f"INSERT INTO {dest_table} VALUES %s", rows)
 
-        dest_table = f"st_{table}"
-        dest_cursor.execute(
-            sql.SQL("SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema='sta' AND table_name={})")
-                .format(sql.Literal(dest_table))
-        )
-        table_exists = dest_cursor.fetchone()[0]
-
-        if table_exists:
-            dest_cursor.execute(sql.SQL("TRUNCATE TABLE {}.{}").format(sql.Identifier('sta'), sql.Identifier(dest_table)))
-        else:
-            source_cursor.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_schema='dbs' AND table_name=%s", (table,))
-            columns = source_cursor.fetchall()
-
-            create_table_query = sql.SQL("CREATE TABLE IF NOT EXISTS {}.{} (").format(sql.Identifier('sta'), sql.Identifier(dest_table))
-            column_definitions = []
-            for column in columns:
-                column_name, data_type = column
-                column_definitions.append(sql.SQL("{} {}").format(sql.Identifier(column_name), sql.SQL(data_type)))
-            create_table_query += sql.SQL(", ").join(column_definitions) + sql.SQL(")")
-            dest_cursor.execute(create_table_query)
-
-        insert_statement = sql.SQL("INSERT INTO {}.{} VALUES %s").format(sql.Identifier('sta'), sql.Identifier(dest_table))
-
-        execute_values(dest_cursor, insert_statement, rows)
-
-    dest_conn.commit()
-    source_cursor.close()
-    source_conn.close()
-    dest_cursor.close()
-    dest_conn.close()
+        dest_conn.commit()
 
 with DAG(
         dag_id='dw_el_job',
