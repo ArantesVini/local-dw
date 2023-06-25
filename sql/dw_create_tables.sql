5048206fc64f6392813b594f809be4cb654708d5
ArantesVini <vinicius.daniel02@gmail.com>
2023-06-25 13:59:05 -0300

    fix: reference the schema in the FKs

--- sql/dw_create_tables.sql
+++ sql/dw_create_tables.sql
@@ -46,8 +46,8 @@ CREATE TABLE dw.fact_sales (
   price_product DECIMAL(10,2) NOT NULL,
   sales_revenue DECIMAL(10,2) NOT NULL,
   PRIMARY KEY (sk_product, sk_customer, sk_locale, sk_time),
-  FOREIGN KEY (sk_product) REFERENCES d_product (sk_product),
-  FOREIGN KEY (sk_customer) REFERENCES d_customer (sk_customer),
-  FOREIGN KEY (sk_locale) REFERENCES d_locale (sk_locale),
-  FOREIGN KEY (sk_time) REFERENCES d_time (sk_time)
+  FOREIGN KEY (sk_product) REFERENCES dw.d_product (sk_product),
+  FOREIGN KEY (sk_customer) REFERENCES dw.d_customer (sk_customer),
+  FOREIGN KEY (sk_locale) REFERENCES dw.d_locale (sk_locale),
+  FOREIGN KEY (sk_time) REFERENCES dw.d_time (sk_time)
 );
