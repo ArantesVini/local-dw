fed3a38dd6be9b29111cbf70d8b4f13126cce15e
ArantesVini <vinicius.daniel02@gmail.com>
2023-06-28 18:47:52 -0300

    add a sql script to inser a new repor metric: sale result, in order to optmize query

--- sql/add_report_metrics.sql
+++ sql/add_report_metrics.sql
@@ -1,3 +1,10 @@
+TRUNCATE TABLE dw.fact_sales;
+
+ALTER TABLE
+    IF EXISTS dw.fact_sales
+ADD
+    COLUMN IF NOT EXISTS sales_result numeric(10, 2) NOT NULL;
+
 INSERT INTO
     dw.fact_sales (
         sk_product,
@@ -7,7 +14,8 @@ INSERT INTO
         quantity,
         price_sale,
         price_product,
-        sales_revenue
+        sales_revenue,
+        sales_result
     )
 SELECT
     sk_product,
@@ -24,7 +32,15 @@ SELECT
             ),
             2
         )
-    ) AS sales_revenue
+    ) AS sales_revenue,
+    SUM(
+        ROUND (
+            (
+                CAST(sale_quantity AS numeric) * CAST(sale_price AS numeric)
+            ),
+            2
+        ) - product_cost
+    ) AS sales_result
 FROM
     sta.sta_sr_sales tb1,
     sta.sta_sr_customers tb2,
@@ -38,7 +54,8 @@ WHERE
     tb2.customer_id = tb1.customer_id
     AND tb3.locality_id = tb1.locality_id
     AND tb4.product_id = tb1.sale_product
-    AND tb1.sale_date = tb5.time_date -- erro é aqui
+    AND to_char(tb1.sale_date, 'YYYY-MM-DD') = to_char(tb5.time_date, 'YYYY-MM-DD')
+    AND to_char(tb1.sale_date, 'HH') = tb5.time_hour
     AND tb2.customer_id = tb8.customer_id
     AND tb3.locality_id = tb7.locale_id
     AND tb4.product_id = tb6.product_id
